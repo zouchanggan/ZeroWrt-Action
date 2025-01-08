fefd2c836735f79c2478578864e2e2e718682239
oppen321 <zj18139624826@gmail.com>
2025-01-08 12:47:53 +0800

    Update Change-source-address.sh

--- scripts/Change-source-address.sh
+++ scripts/Change-source-address.sh
@@ -4,11 +4,11 @@ mkdir -p files/etc/opkg
 
 # 设置新的源地址
 NEW_SOURCES="
-src/gz openwrt_base https://mirrors.tencent.com/lede/releases/18.06.9/packages/x86_64/base
-src/gz openwrt_luci https://mirrors.tencent.com/lede/releases/18.06.9/packages/x86_64/luci
-src/gz openwrt_packages https://mirrors.tencent.com/lede/releases/18.06.9/packages/x86_64/packages
-src/gz openwrt_routing https://mirrors.tencent.com/lede/releases/18.06.9/packages/x86_64/routing
-src/gz openwrt_telephony https://mirrors.tencent.com/lede/releases/18.06.9/packages/x86_64/telephony
+src/gz openwrt_base https://downloads.openwrt.org/releases/24.10.0-rc5/packages/x86_64/base
+src/gz openwrt_luci https://downloads.openwrt.org/releases/24.10.0-rc5/packages/x86_64/luci
+src/gz openwrt_packages https://downloads.openwrt.org/releases/24.10.0-rc5/packages/x86_64/packages
+src/gz openwrt_routing https://downloads.openwrt.org/releases/24.10.0-rc5/packages/x86_64/routing
+src/gz openwrt_telephony https://downloads.openwrt.org/releases/24.10.0-rc5/packages/x86_64/telephony
 "
 
 # 创建 opkg 配置文件并写入源地址
