b783278e19e78abf283c9d4c682d9c8125f017fe
oppen321 <zj18139624826@gmail.com>
2025-01-08 12:32:25 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -39,25 +39,25 @@ function git_sparse_clone() {
 
 # golong1.23依赖
 #git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/packages_lang_golang -b 23.x feeds/packages/lang/golang
+git clone https://git.kejizero.online/zhao/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 # SSRP & Passwall
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/openwrt_helloworld.git package/helloworld -b v5
+git clone https://git.kejizero.online/zhao/openwrt_helloworld.git package/helloworld -b v5
 
 # Alist
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/luci-app-alist package/alist
+git clone https://git.kejizero.online/zhao/luci-app-alist package/alist
 
 # Mosdns
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/luci-app-mosdns.git -b v5 package/mosdns
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/v2ray-geodata.git package/v2ray-geodata
+git clone https://git.kejizero.online/zhao/luci-app-mosdns.git -b v5 package/mosdns
+git clone https://git.kejizero.online/zhao/v2ray-geodata.git package/v2ray-geodata
 
 # Realtek 网卡 - R8168 & R8125 & R8126 & R8152 & R8101
 rm -rf package/kernel/r8168 package/kernel/r8101 package/kernel/r8125 package/kernel/r8126
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/package_kernel_r8168 package/kernel/r8168
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/package_kernel_r8152 package/kernel/r8152
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/package_kernel_r8101 package/kernel/r8101
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/package_kernel_r8125 package/kernel/r8125
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/package_kernel_r8126 package/kernel/r8126
+git clone https://git.kejizero.online/zhao/package_kernel_r8168 package/kernel/r8168
+git clone https://git.kejizero.online/zhao/package_kernel_r8152 package/kernel/r8152
+git clone https://git.kejizero.online/zhao/package_kernel_r8101 package/kernel/r8101
+git clone https://git.kejizero.online/zhao/package_kernel_r8125 package/kernel/r8125
+git clone https://git.kejizero.online/zhao/package_kernel_r8126 package/kernel/r8126
 
 # Adguardhome
 git_sparse_clone master https://github.com/kenzok8/openwrt-packages adguardhome luci-app-adguardhome
@@ -68,12 +68,12 @@ git_sparse_clone main https://github.com/linkease/istore luci
 
 # Docker
 rm -rf feeds/luci/applications/luci-app-dockerman
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/luci-app-dockerman -b 24.10 feeds/luci/applications/luci-app-dockerman
+git clone https://git.kejizero.online/zhao/luci-app-dockerman -b 24.10 feeds/luci/applications/luci-app-dockerman
 rm -rf feeds/packages/utils/{docker,dockerd,containerd,runc}
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/packages_utils_docker feeds/packages/utils/docker
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/packages_utils_dockerd feeds/packages/utils/dockerd
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/packages_utils_containerd feeds/packages/utils/containerd
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/packages_utils_runc feeds/packages/utils/runc
+git clone https://git.kejizero.online/zhao/packages_utils_docker feeds/packages/utils/docker
+git clone https://git.kejizero.online/zhao/packages_utils_dockerd feeds/packages/utils/dockerd
+git clone https://git.kejizero.online/zhao/packages_utils_containerd feeds/packages/utils/containerd
+git clone https://git.kejizero.online/zhao/packages_utils_runc feeds/packages/utils/runc
 sed -i '/sysctl.d/d' feeds/packages/utils/dockerd/Makefile
 pushd feeds/packages
     curl -s https://init.cooluc.com/openwrt/patch/docker/0001-dockerd-fix-bridge-network.patch | patch -p1
@@ -83,6 +83,66 @@ popd
 
 # UPnP
 rm -rf feeds/{packages/net/miniupnpd,luci/applications/luci-app-upnp}
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/miniupnpd feeds/packages/net/miniupnpd -b v2.3.7
-git clone https://$GIT_USERNAME:$GIT_PASSWORD@git.kejizero.online/zhao/luci-app-upnp feeds/luci/applications/luci-app-upnp -b master
+git clone https://git.kejizero.online/zhao/miniupnpd feeds/packages/net/miniupnpd -b v2.3.7
+git clone https://git.kejizero.online/zhao/luci-app-upnp feeds/luci/applications/luci-app-upnp -b master
 
+# Zero-package
+git clone --depth=1 https://github.com/oppen321/Zero-package package/Zero-package
+
+# 一键配置拨号
+git clone --depth=1 https://github.com/sirpdboy/luci-app-netwizard package/luci-app-netwizard
+sed -i 's/\("admin"\), *\("netwizard"\)/\1, "system", \2/g' package/luci-app-netwizard/luasrc/controller/*.lua
+
+# 修改名称
+sed -i 's/OpenWrt/ZeroWrt/' package/base-files/files/bin/config_generate
+
+# default settings
+git clone https://git.kejizero.online/zhao/default-settings package/new/default-settings -b openwrt-24.10
+
+# Theme
+git clone https://git.kejizero.online/zhao/luci-theme-argon.git package/new/luci-theme-argon
+cp -f $GITHUB_WORKSPACE/images/bg.webp package/new/luci-theme-argon/luci-theme-argon/htdocs/luci-static/argon/img/bg.webp
+
+# 更换背景
+curl -L -o package/new/luci-theme-argon/luci-theme-argon/htdocs/luci-static/argon/img/bg.webp https://git.kejizero.online/zhao/files/raw/branch/main/%20background/bg.webp
+
+# OpenAppFilter
+git clone https://git.kejizero.online/zhao/OpenAppFilter --depth=1 package/OpenAppFilter
+
+# luci-app-partexp
+git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp package/luci-app-partexp
+
+# luci-app-webdav
+git clone https://git.kejizero.online/zhao/luci-app-webdav package/new/luci-app-webdav
+
+# unzip
+rm -rf feeds/packages/utils/unzip
+git clone https://github.com/sbwml/feeds_packages_utils_unzip feeds/packages/utils/unzip
+
+# frpc名称
+sed -i 's,发送,Transmission,g' feeds/luci/applications/luci-app-transmission/po/zh_Hans/transmission.po
+sed -i 's,frp 服务器,FRP 服务器,g' feeds/luci/applications/luci-app-frps/po/zh_Hans/frps.po
+sed -i 's,frp 客户端,FRP 客户端,g' feeds/luci/applications/luci-app-frpc/po/zh_Hans/frpc.po
+
+# 必要的补丁
+pushd feeds/luci
+    curl -s https://raw.githubusercontent.com/oppen321/path/refs/heads/main/Firewall/0001-luci-mod-status-firewall-disable-legacy-firewall-rul.patch | patch -p1
+popd
+
+# NTP
+sed -i 's/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/1.openwrt.pool.ntp.org/ntp2.aliyun.com/g' package/base-files/files/bin/config_generate
+sed -i 's/2.openwrt.pool.ntp.org/time1.cloud.tencent.com/g' package/base-files/files/bin/config_generate
+sed -i 's/3.openwrt.pool.ntp.org/time2.cloud.tencent.com/g' package/base-files/files/bin/config_generate
+
+# ZeroWrt选项菜单
+mkdir -p files/bin
+# cp -f $GITHUB_WORKSPACE/Diy/ZeroWrt files/bin/ZeroWrt
+curl -L -o files/bin/ZeroWrt https://git.kejizero.online/zhao/files/raw/branch/main/bin/ZeroWrt
+chmod +x files/bin/ZeroWrt
+mkdir -p files/root
+curl -L -o files/root/version.txt https://git.kejizero.online/zhao/files/raw/branch/main/bin/version.txt
+chmod +x files/root/version.txt
+
+./scripts/feeds update -a
+./scripts/feeds install -a
