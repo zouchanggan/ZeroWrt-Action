e4ca20cf04bf8405c5e55917a936f768c1721d17
oppen321 <zj18139624826@gmail.com>
2025-01-09 23:12:57 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -148,5 +148,9 @@ mkdir -p files/root
 curl -L -o files/root/version.txt https://git.kejizero.online/zhao/files/raw/branch/main/bin/version.txt
 chmod +x files/root/version.txt
 
+# Adguardhome设置
+mkdir -p files/etc
+curl -L -o files/etc/AdGuardHome-dnslist https://git.kejizero.online/zhao/files/raw/branch/main/etc/AdGuardHome-dnslist
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
