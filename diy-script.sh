6e55f954682f3a9577980d8f15e11fd109af5e1a
oppen321 <zj18139624826@gmail.com>
2025-01-10 13:36:31 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -127,7 +127,6 @@ sed -i 's,frp 客户端,FRP 客户端,g' feeds/luci/applications/luci-app-frpc/p
 
 # 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
-package/base-files/files/etc/openwrt_release
 orig_version=$(cat "package/base-files/files/etc/openwrt_release" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by OPPEN321/g" package/base-files/files/etc/openwrt_release
 
