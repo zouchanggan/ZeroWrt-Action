a9a4d713d20e2ad37809e811ba9c3f51ad8de687
oppen321 <zj18139624826@gmail.com>
2025-01-10 09:45:38 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -246,6 +246,7 @@ uci commit AdGuardHome
 /etc/init.d/AdGuardHome restart
 
 # 设置Adguardhome上游服务器
+chmod 644 /etc/AdGuardHome.yaml
 # sed -i 's|- 223.5.5.5|- 127.0.0.1:6053|' /etc/AdGuardHome.yaml
 # sed -i '/upstream_dns:/a\    - 127.0.0.1:5335' /etc/AdGuardHome.yaml
 sed -i 's|upstream_dns_file: ""|upstream_dns_file: "/etc/AdGuardHome-dnslist.yaml"|' /etc/AdGuardHome.yaml
