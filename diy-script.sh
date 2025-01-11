bc21fe64c2c0e44b3795566a9945c06247051936
oppen321 <zj18139624826@gmail.com>
2025-01-11 22:50:51 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+shopt -s extglob
+SHELL_FOLDER=$(dirname $(readlink -f "$0"))
+
 # 修改默认IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
@@ -22,9 +25,28 @@ curl -so files/root/.bash_profile https://git.kejizero.online/zhao/files/raw/bra
 curl -so files/root/.bashrc https://git.kejizero.online/zhao/files/raw/branch/main/root/.bashrc
 
 # 切换linux
-git clone -b openwrt-24.10 --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt immortalwrt
-rm -rf ./target/linux
-cp -r immortalwrt/target/linux ./target
+rm -rf package/boot target/linux/rockchip target/linux/generic
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/rockchip target/linux/generic package/boot
+
+# 加载补丁
+wget -N https://github.com/istoreos/istoreos/raw/istoreos-22.03/target/linux/rockchip/patches-5.10/305-r2s-pwm-fan.patch -P target/linux/rockchip/patches-6.6/
+wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.10/target/linux/rockchip/Makefile -P target/linux/rockchip/
+wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-6.6 -P include/
+wget -N https://github.com/coolsnowwolf/lede/raw/refs/heads/master/include/trusted-firmware-a.mk -P include/
+sed -i "/KernelPackage,ptp/d" package/kernel/linux/modules/other.mk
+wget -N https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/16414.patch -P devices/common/patches/
+
+# 移除无用文件
+mv -f tmp/r8125 feeds/oppen321/
+rm -rf target/linux/rockchip/armv8/base-files/etc/uci-defaults/13_opkg_update package/feeds/oppen321/pcat-manager
+sed -i -e 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
+sed -i -e 's,wpad-openssl,wpad-basic-mbedtls,g' target/linux/rockchip/image/armv8.mk
+sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += fdisk lsblk kmod-drm-rockchip/' target/linux/rockchip/Makefile
+cp -Rf $SHELL_FOLDER/diy/* ./
+sed -i 's/Ariaboard/光影猫/' target/linux/rockchip/image/armv8.mk
+echo '
+CONFIG_SENSORS_PWM_FAN=y
+' >> ./target/linux/rockchip/armv8/config-6.6
 
 # 移除要替换的包
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardhome,socat}
