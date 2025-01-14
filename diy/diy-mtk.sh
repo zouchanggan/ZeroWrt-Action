189a73afa42e573b522cc83b1cda7f25a303ee59
oppen321 <zj18139624826@gmail.com>
2025-01-14 17:29:15 +0800

    Update diy-mtk.sh

--- diy/diy-mtk.sh
+++ diy/diy-mtk.sh
@@ -13,6 +13,10 @@ echo -e "msgstr \"魔法网络\"" >> feeds/luci/modules/luci-base/po/zh_Hans/bas
 ##配置IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
+# 更改 Argon 主题背景
+cp -f $GITHUB_WORKSPACE/images/bg1.jpg feeds/theme/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+sed -i "s/option online_wallpaper 'bing'/option online_wallpaper 'none'/g" feeds/luci/applications/luci-app-argon-config/root/etc/config/argon
+
 ##
 rm -rf ./feeds/extraipk/theme/luci-theme-argon-18.06
 rm -rf ./feeds/extraipk/theme/luci-app-argon-config-18.06
