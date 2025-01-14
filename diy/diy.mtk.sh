dc6ebcb42f988213bb305cc9a88b6359370e0406
oppen321 <zj18139624826@gmail.com>
2025-01-14 11:31:47 +0800

    Update diy.mtk.sh

--- diy/diy.mtk.sh
+++ diy/diy.mtk.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-#=================================================
-# DaoDao's script
-#=================================================             
+#!/bin/bash          
 
+##添加自己的插件库
+echo -e "\nsrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk" >> feeds.conf.default
 
 ##
 echo -e "\nmsgid \"Control\"" >> feeds/luci/modules/luci-base/po/zh_Hans/base.po
