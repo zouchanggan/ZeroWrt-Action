0dc73f6445e51349112d2d0e3a20b7101b045e66
oppen321 <zj18139624826@gmail.com>
2025-01-14 11:08:22 +0800

    Update diy.mtk.sh

--- diy/diy.mtk.sh
+++ diy/diy.mtk.sh
@@ -21,40 +21,9 @@ mkdir -p files/root
 curl -so files/root/.bash_profile https://git.kejizero.online/zhao/files/raw/branch/main/root/.bash_profile
 curl -so files/root/.bashrc https://git.kejizero.online/zhao/files/raw/branch/main/root/.bashrc
 
-# 移除要替换的包
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardhome,socat}
-rm -rf feeds/packages/net/alist feeds/luci/applications/luci-app-alist
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
-
 # golong1.23依赖
 #git clone --depth=1 https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 git clone https://git.kejizero.online/zhao/packages_lang_golang -b 23.x feeds/packages/lang/golang
-
-# Alist
-git clone https://git.kejizero.online/zhao/luci-app-alist package/alist
-
-# iStore
-git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-git_sparse_clone main https://github.com/linkease/istore luci
-
-# UPnP
-rm -rf feeds/{packages/net/miniupnpd,luci/applications/luci-app-upnp}
-git clone https://git.kejizero.online/zhao/miniupnpd feeds/packages/net/miniupnpd -b v2.3.7
-git clone https://git.kejizero.online/zhao/luci-app-upnp feeds/luci/applications/luci-app-upnp -b master
-
-# Zero-package
-git clone --depth=1 https://github.com/oppen321/Zero-package package/Zero-package
 
 # 一键配置拨号
 git clone --depth=1 https://github.com/sirpdboy/luci-app-netwizard package/luci-app-netwizard
@@ -68,39 +37,13 @@ git clone https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat 
 # default-settings
 git clone --depth=1 -b main https://github.com/oppen321/default-settings package/default-settings
 
-# Lucky
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-
-# OpenAppFilter
-git clone https://git.kejizero.online/zhao/OpenAppFilter --depth=1 package/OpenAppFilter
-
-# luci-app-partexp
-git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp package/luci-app-partexp
-
 # 进阶设置
 git clone https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
-
-# luci-app-webdav
-git clone https://git.kejizero.online/zhao/luci-app-webdav package/new/luci-app-webdav
-
-# unzip
-rm -rf feeds/packages/utils/unzip
-git clone https://github.com/sbwml/feeds_packages_utils_unzip feeds/packages/utils/unzip
-
-# frpc名称
-sed -i 's,发送,Transmission,g' feeds/luci/applications/luci-app-transmission/po/zh_Hans/transmission.po
-sed -i 's,frp 服务器,FRP 服务器,g' feeds/luci/applications/luci-app-frps/po/zh_Hans/frps.po
-sed -i 's,frp 客户端,FRP 客户端,g' feeds/luci/applications/luci-app-frpc/po/zh_Hans/frpc.po
 
 # 修改版本为编译日期
 date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/base-files/files/etc/openwrt_release" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by OPPEN321/g" package/base-files/files/etc/openwrt_release
-
-# 必要的补丁
-pushd feeds/luci
-    curl -s https://raw.githubusercontent.com/oppen321/path/refs/heads/main/Firewall/0001-luci-mod-status-firewall-disable-legacy-firewall-rul.patch | patch -p1
-popd
 
 # NTP
 sed -i 's/0.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
