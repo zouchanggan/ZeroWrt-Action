cf337edc3a60eb5dc8fe660b25b7cd60cfd68793
OPPEN321 <zj18139624826@gmail.com>
2025-01-24 20:52:48 +0800

    Update diy-istoreos.sh

--- diy/diy-istoreos.sh
+++ diy/diy-istoreos.sh
@@ -162,5 +162,20 @@ curl -L -o files/etc/sysctl.d/16-udp-buffer-size.conf https://raw.githubusercont
 chmod +x files/etc/sysctl.d/15-vm-swappiness.conf
 chmod +x files/etc/sysctl.d/16-udp-buffer-size.conf
 
+# default_set
+mkdir -p files/etc/config
+curl -L -o files/etc/config/default_dhcp.conf https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_dhcp.conf
+curl -L -o files/etc/config/default_mosdns https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_mosdns
+curl -L -o files/etc/config/default_smartdns https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_smartdns
+curl -L -o files/etc/config/default_AdGuardHome https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_AdGuardHome
+curl -L -o files/etc/config/default_passwall https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_passwall
+curl -L -o files/etc/config/default_openclash https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_openclash
+chmod +x files/etc/config/default_dhcp.conf
+chmod +x files/etc/config/default_mosdns
+chmod +x files/etc/config/default_smartdns
+chmod +x files/etc/config/default_AdGuardHome
+chmod +x files/etc/config/default_passwall
+chmod +x files/etc/config/default_openclash
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
