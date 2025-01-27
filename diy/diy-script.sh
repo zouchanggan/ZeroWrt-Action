3213f9c13ca85c1235b5fb254c3e00140ca6087a
OPPEN321 <zj18139624826@gmail.com>
2025-01-27 11:03:08 +0800

    Update diy-script.sh

--- diy/diy-script.sh
+++ diy/diy-script.sh
@@ -90,6 +90,18 @@ git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/l
 # SSRP & Passwall
 git clone https://git.kejizero.online/zhao/openwrt_helloworld.git package/helloworld -b v5
 
+# nginx
+rm -rf feeds/packages/net/nginx
+git clone https://github.com/sbwml/feeds_packages_net_nginx -b openwrt-24.10 feeds/packages/net/nginx
+
+# nginx - config
+curl -s https://init2.cooluc.com/openwrt/nginx/luci.locations > feeds/packages/net/nginx/files-luci-support/luci.locations
+curl -s https://init2.cooluc.com/openwrt/nginx/uci.conf.template > feeds/packages/net/nginx-util/files/uci.conf.template
+
+# opkg
+mkdir -p package/system/opkg/patches
+curl -s https://init2.cooluc.com/openwrt/patch/opkg/900-opkg-download-disable-hsts.patch > package/system/opkg/patches/900-opkg-download-disable-hsts.patch
+
 # Alist
 git clone https://git.kejizero.online/zhao/luci-app-alist package/alist
 
