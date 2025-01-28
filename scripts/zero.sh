42007cc65c40647ba9c16b23a2dafd77a6a76a5a
OPPEN321 <zj18139624826@gmail.com>
2025-01-28 19:25:16 +0800

    Update zero.sh

--- scripts/zero.sh
+++ scripts/zero.sh
@@ -33,4 +33,4 @@ chmod +x files/etc/config/default_passwall
 chmod +x files/etc/config/default_openclash
 
 # 更新default-settings
-cp -f $GITHUB_WORKSPACE/files/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
+curl -L -o package/lean/default-settings/files/zzz-default-settings https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/zzz-default-settings
