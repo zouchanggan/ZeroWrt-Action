66415b8311386308dcc31bc76039ca601f90d3da
OPPEN321 <zj18139624826@gmail.com>
2025-01-29 09:36:35 +0800

    Update diy-bypass.sh

--- diy/diy-bypass.sh
+++ diy/diy-bypass.sh
@@ -113,3 +113,4 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages packa
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+
