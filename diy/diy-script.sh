2a1d6bcf5803632e431d6238ed2f8f33ed57ffe7
OPPEN321 <zj18139624826@gmail.com>
2025-02-02 02:53:33 +0800

    Update diy-script.sh

--- diy/diy-script.sh
+++ diy/diy-script.sh
@@ -131,9 +131,6 @@ git_sparse_clone master https://github.com/linkease/nas-packages network/service
 git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
 git_sparse_clone main https://github.com/linkease/istore luci
 
-# istoreos-files
-git clone https://github.com/oppen321/istoreos-files package/istoreos-files
-
 # Lucky
 git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
