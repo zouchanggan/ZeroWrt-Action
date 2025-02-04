29930d3e3d5d5229178de1a588d2fefd41fc49dd
OPPEN321 <zj18139624826@gmail.com>
2025-02-04 23:56:37 +0800

    Update zero.sh

--- scripts/zero.sh
+++ scripts/zero.sh
@@ -19,12 +19,12 @@ chmod +x files/etc/AdGuardHome-dns.yaml
 
 # default_set
 mkdir -p files/etc/config
-curl -L -o files/etc/config/default_dhcp.conf https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_dhcp.conf
-curl -L -o files/etc/config/default_mosdns https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_mosdns
-curl -L -o files/etc/config/default_smartdns https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_smartdns
-curl -L -o files/etc/config/default_AdGuardHome https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_AdGuardHome
-curl -L -o files/etc/config/default_passwall https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_passwall
-curl -L -o files/etc/config/default_openclash https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/master/files/default_openclash
+curl -L -o files/etc/config/default_dhcp.conf https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/openwrt-24.10/files/default_dhcp.conf
+curl -L -o files/etc/config/default_mosdns https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/openwrt-24.10/files/default_mosdns
+curl -L -o files/etc/config/default_smartdns https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/openwrt-24.10/files/default_smartdns
+curl -L -o files/etc/config/default_AdGuardHome https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/openwrt-24.10/files/default_AdGuardHome
+curl -L -o files/etc/config/default_passwall https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/openwrt-24.10/files/default_passwall
+curl -L -o files/etc/config/default_openclash https://raw.githubusercontent.com/oppen321/ZeroWrt/refs/heads/openwrt-24.10/files/default_openclash
 chmod +x files/etc/config/default_dhcp.conf
 chmod +x files/etc/config/default_mosdns
 chmod +x files/etc/config/default_smartdns
