17f7e787263671ce87b01529145c5199e5519695
OPPEN321 <zj18139624826@gmail.com>
2025-02-16 22:48:38 +0800

    Update diy-script.sh

--- diy/diy-script.sh
+++ diy/diy-script.sh
@@ -157,7 +157,7 @@ sed -i 's/bing/none/' package/luci-app-argon-config/root/etc/config/argon
 git clone --depth=1 -b openwrt-24.10 https://github.com/oppen321/default-settings package/default-settings
 
 # Lucky
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 # OpenAppFilter
 git clone https://git.kejizero.online/zhao/OpenAppFilter --depth=1 package/OpenAppFilter
