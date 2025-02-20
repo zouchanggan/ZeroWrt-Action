b4107df37602cec4b60216aa70066ec4448c9afd
OPPEN321 <zj18139624826@gmail.com>
2025-02-20 22:55:19 +0800

    Create diy-lean.sh

---
+++ diy/diy-lean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 修改版本为编译日期
+date_version=$(date +"%y.%m.%d")
+orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
+sed -i "s/${orig_version}/R${date_version} by OPPPEN321/g" package/lean/default-settings/files/zzz-default-settings
