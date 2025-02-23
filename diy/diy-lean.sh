e48bfa7a1b379867fc7e0c122362b1a078411869
NERD <168670760+zhiern@users.noreply.github.com>
2025-02-23 21:03:39 +0800

    Update diy-lean.sh

--- diy/diy-lean.sh
+++ diy/diy-lean.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Change default ip
-
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/luci2/bin/config_generate
 
 # Change hostname
 sed -i "s/hostname='.*'/hostname='ZeroWrt'/g" package/base-files/files/bin/config_generate
