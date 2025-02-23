72e3695212081968323980cb990c07a9402383e8
NERD <168670760+zhiern@users.noreply.github.com>
2025-02-23 20:46:03 +0800

    Update diy-lean.sh

--- diy/diy-lean.sh
+++ diy/diy-lean.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Remove and update plugins
-rm -rf 
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box,adguardhome,alist,chinadns-ng,daed,dns2socks,dns2tcp,pdnsd-alt,shadowsocks-libev,trojan,v2ray-geodata,}
+rm -rf feeds/luci/applications/{luci-app-alist,luci-app-daed,luci-app-mosdns,luci-app-openclash,luci-app-passwall,luci-app-passwall2}
+rm -rf feeds/packages/lang/golang
 
 # OpenAppFilter
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
