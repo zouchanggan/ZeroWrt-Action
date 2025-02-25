9d30e34289e73faab6ddeb4952936d3abfdaed84
OPPEN321 <zj18139624826@gmail.com>
2025-02-25 10:59:15 +0800

    Update diy-lean.sh

--- diy/diy-lean.sh
+++ diy/diy-lean.sh
@@ -8,6 +8,9 @@ sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/luci2/bin/config_generate
 sed -i "s/hostname='.*'/hostname='ZeroWrt'/g" package/base-files/files/bin/config_generate
 sed -i "s/hostname='.*'/hostname='ZeroWrt'/g" package/base-files/luci2/bin/config_generate
 
+# banner
+cp -f $GITHUB_WORKSPACE/diy/banner  package/base-files/files/etc/banner
+
 # Replace kernel
 sed -i 's/6.12/6.6/g' target/linux/x86/Makefile
 
