5876dd2a58b0d614c98eb0f15e1b77f1d42f9e98
OPPEN321 <zj18139624826@gmail.com>
2025-03-02 17:31:53 +0800

    Update diy-lean.sh

--- diy/diy-lean.sh
+++ diy/diy-lean.sh
@@ -38,6 +38,9 @@ function git_sparse_clone() {
 # Golang
 git clone -b 23.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 
+# adguardhome
+git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+
 # OpenAppFilter
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
