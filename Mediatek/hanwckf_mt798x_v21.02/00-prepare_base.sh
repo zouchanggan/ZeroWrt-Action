154c88d248623b46b364ee7d2643a8a3ff6c91ee
OPPEN321 <zj18139624826@gmail.com>
2025-03-19 01:47:58 +0800

    Create 00-prepare_base.sh

---
+++ Mediatek/hanwckf_mt798x_v21.02/00-prepare_base.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/oppen321/ZeroWrt-Action
+# Description: Automatically Build OpenWrt for Mediatek
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/padavanonly/immortalwrt-mt798x-24.10 / Branch: 2410
+#========================================================================================================================
+
+sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
