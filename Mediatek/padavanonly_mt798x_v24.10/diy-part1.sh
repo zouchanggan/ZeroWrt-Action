8ed266458e4564c181afca0d388e71949ed39321
OPPEN321 <zj18139624826@gmail.com>
2025-03-18 21:06:58 +0800

    Update diy-part1.sh

--- Mediatek/padavanonly_mt798x_v24.10/diy-part1.sh
+++ Mediatek/padavanonly_mt798x_v24.10/diy-part1.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/oppen321/ZeroWrt-Action
+# Description: Automatically Build OpenWrt for Mediatek
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/padavanonly/immortalwrt-mt798x-24.10 / Branch: 2410
+#========================================================================================================================
 
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# other
+# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
