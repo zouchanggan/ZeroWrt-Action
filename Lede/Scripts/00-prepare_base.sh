d146c48e3d07958946adc1fad78bd9b7cfa05e7b
OPPEN321 <zj18139624826@gmail.com>
2025-03-19 17:43:51 +0800

    Update 00-prepare_base.sh

--- Lede/Scripts/00-prepare_base.sh
+++ Lede/Scripts/00-prepare_base.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/oppen321/ZeroWrt-Action
+# Description: Automatically Build OpenWrt for Rockchip
+# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
+#========================================================================================================================
 
