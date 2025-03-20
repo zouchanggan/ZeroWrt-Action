634b68fff2d54ea46f16c2f97ede01af4466fa34
OPPEN321 <zj18139624826@gmail.com>
2025-03-20 21:13:22 +0800

    Update 01-prepare_package.sh

--- Mediatek/hanwckf_mt798x_v21.02/01-prepare_package.sh
+++ Mediatek/hanwckf_mt798x_v21.02/01-prepare_package.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
 # 更新插件
-rm -rf ./feeds/packages/net/{dns2socks,v2ray-core,xray-core,pdnsd-alt,dns2tcp,ipt2socks,microsocks,chinadns-ng,redsocks2,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,trojan,tuic-client,v2ray-geodata,v2ray-plugin,xray-plugin}
-rm -rf ./feeds/luci/applications/{luci-app-passwall,luci-app-openclash,luci-app-ssr-plus}
-rm -rf ./package/feeds/packages/{dns2socks,v2ray-core,xray-core,pdnsd-alt,dns2tcp,ipt2socks,microsocks,chinadns-ng,redsocks2,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,trojan,tuic-client,v2ray-geodata,v2ray-plugin,xray-plugin}
-rm -rf ./package/feeds/luci/{luci-app-passwall,luci-app-openclash,luci-app-ssr-plus}
-rm -rf feeds/packages/lang/golang
+rm -rf feeds/luci/applications/luci-app-passwall/*
+rm -rf feeds/luci/applications/luci-app-ssr-plus/*
+rm -rf feeds/luci/applications/luci-app-openclash/*
+rm -rf feeds/packages/lang/golang/*
 
-# 加载软件源
-git clone --depth=1 -b helloworld https://github.com/oppen321/openwrt-package package/openwrt-package
+# luci-app-passwall
+rm -rf feeds/luci/applications/luci-app-passwall/*
+git clone --depth=1 https://github.com/oppen321/luci-app-passwall package/luci-app-passwall
+cp -af package/luci-app-passwall/*  feeds/luci/applications/luci-app-passwall/
+
+# luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-ssr-plus/*
+git clone --depth=1 https://github.com/oppen321/luci-app-ssr-plus package/luci-app-ssr-plus
+cp -af package/luci-app-ssr-plus/*  feeds/luci/applications/luci-app-ssr-plus/
+
+# luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-openclash/*
 
 # luci-app-adguardhome
-git clone --depth=1 https://github.com/oppen321/luci-app-adguardhome package/luci-app-adguardhome
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
 # 设置文件目录
 mv package/openwrt-package/{dns2socks,v2ray-core,xray-core,pdnsd,dns2tcp,ipt2socks,microsocks,chinadns-ng,redsocks2,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,trojan,tuic-client,v2ray-geodata,v2ray-plugin,xray-plugin} feeds/packages/net
