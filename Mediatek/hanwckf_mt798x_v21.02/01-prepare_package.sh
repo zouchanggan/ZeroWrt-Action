d43fe12d8510e4cd692f2b95f6bbef50d4c3ff6d
OPPEN321 <zj18139624826@gmail.com>
2025-03-21 00:22:05 +0800

    Update 01-prepare_package.sh

--- Mediatek/hanwckf_mt798x_v21.02/01-prepare_package.sh
+++ Mediatek/hanwckf_mt798x_v21.02/01-prepare_package.sh
@@ -32,10 +32,12 @@ cp -af package/golang/* feeds/packages/lang/golang/
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
 # luci-app-mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-rm -rf feeds/package/net/mosdns
+rm -rf feeds/packages/net/v2ray-geodata/*
+rm -rf feeds/package/net/mosdns/*
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
 git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+cp -af package/mosdns/mosdns/* feeds/package/net/mosdns/
+cp -af package/v2ray-geodata/* feeds/packages/net/v2ray-geodata/
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
