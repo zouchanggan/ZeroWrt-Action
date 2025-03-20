04d2678832783b714a3654226cec7f556ef41514
OPPEN321 <zj18139624826@gmail.com>
2025-03-20 21:15:21 +0800

    Update 01-prepare_package.sh

--- Mediatek/hanwckf_mt798x_v21.02/01-prepare_package.sh
+++ Mediatek/hanwckf_mt798x_v21.02/01-prepare_package.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-# 更新插件
-rm -rf feeds/luci/applications/luci-app-passwall/*
-rm -rf feeds/luci/applications/luci-app-ssr-plus/*
-rm -rf feeds/luci/applications/luci-app-openclash/*
 rm -rf feeds/packages/lang/golang/*
 
 # luci-app-passwall
@@ -19,12 +14,11 @@ cp -af package/luci-app-ssr-plus/*  feeds/luci/applications/luci-app-ssr-plus/
 # luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash/*
 
+## golang 为 1.24.x
+rm -rf feeds/packages/lang/golang/*
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x packages/golang
+cp -af packages/golang/* feeds/packages/lang/golang/
+
 # luci-app-adguardhome
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
-# 设置文件目录
-mv package/openwrt-package/{dns2socks,v2ray-core,xray-core,pdnsd,dns2tcp,ipt2socks,microsocks,chinadns-ng,redsocks2,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,trojan,tuic-client,v2ray-geodata,v2ray-plugin,xray-plugin} feeds/packages/net
-mv package/openwrt-package/{luci-app-passwall,luci-app-openclash,luci-app-ssr-plus} feeds/luci/applications
-
-## golang 为 1.24.x
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
