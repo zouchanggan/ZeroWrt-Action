0d6e29900a14f37a116651ee0f445797faf1b52b
OPPEN321 <zj18139624826@gmail.com>
2025-03-20 13:06:01 +0800

    Update 00-prepare_base.sh

--- Mediatek/hanwckf_mt798x_v21.02/00-prepare_base.sh
+++ Mediatek/hanwckf_mt798x_v21.02/00-prepare_base.sh
@@ -3,9 +3,18 @@
 # https://github.com/oppen321/ZeroWrt-Action
 # Description: Automatically Build OpenWrt for Mediatek
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/padavanonly/immortalwrt-mt798x-24.10 / Branch: 2410
+# Source code repository: https://github.com/hanwckf/immortalwrt-mt798x / Branch: openwrt-21.02
 #========================================================================================================================
 
+# default LAN IP
+sed -i "s/192.168.6.1/$LAN/g" package/base-files/files/bin/config_generate
+
+# init-settings.sh
+mkdir -p files/etc/uci-defaults
+curl -s $mirror/Mediatek/files/etc/uci-defaults/99-init-settings > files/etc/uci-defaults/99-init-settings
+
+# Load source
 sed -i "1isrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk\n" feeds.conf.default
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
