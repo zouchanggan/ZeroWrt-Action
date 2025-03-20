ab4c4e34c2e623111ea64c6a138f60a3e658be07
OPPEN321 <zj18139624826@gmail.com>
2025-03-20 21:21:05 +0800

    Update 01-prepare_package.sh

--- Mediatek/hanwckf_mt798x_v21.02/01-prepare_package.sh
+++ Mediatek/hanwckf_mt798x_v21.02/01-prepare_package.sh
@@ -13,6 +13,8 @@ cp -af package/luci-app-ssr-plus/*  feeds/luci/applications/luci-app-ssr-plus/
 
 # luci-app-openclash
 rm -rf feeds/luci/applications/luci-app-openclash/*
+git clone --depth=1 https://github.com/oppen321/luci-app-openclash package/luci-app-openclash
+cp -af package/luci-app-openclash/*  feeds/luci/applications/luci-app-openclash/
 
 ## golang 为 1.24.x
 rm -rf feeds/packages/lang/golang/*
