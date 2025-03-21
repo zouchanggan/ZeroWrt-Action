909e4e703047dfec48933b85ab36a79692294d2d
OPPEN321 <zj18139624826@gmail.com>
2025-03-21 11:10:22 +0800

    Update 01-prepare_package.sh

--- Mediatek/hanwckf_mt798x_v21.02/01-prepare_package.sh
+++ Mediatek/hanwckf_mt798x_v21.02/01-prepare_package.sh
@@ -1,33 +1,5 @@
 #!/bin/bash
 
-# luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-passwall/*
-git clone --depth=1 https://github.com/oppen321/luci-app-passwall package/luci-app-passwall
-cp -af package/luci-app-passwall/*  feeds/luci/applications/luci-app-passwall/
-
-# luci-app-ssr-plus
-rm -rf feeds/luci/applications/luci-app-ssr-plus/*
-git clone --depth=1 https://github.com/oppen321/luci-app-ssr-plus package/luci-app-ssr-plus
-cp -af package/luci-app-ssr-plus/*  feeds/luci/applications/luci-app-ssr-plus/
-
-# luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-openclash/*
-git clone --depth=1 https://github.com/oppen321/luci-app-openclash package/luci-app-openclash
-cp -af package/luci-app-openclash/*  feeds/luci/applications/luci-app-openclash/
-
-# xray-core
-rm -rf feeds/packages/net/xray-core/*
-git clone --depth=1 https://github.com/oppen321/xray-core package/xray-core
-cp -af package/xray-core/*  feeds/packages/net/xray-core/
-
-# v2ray-core
-rm -rf feeds/packages/net/v2ray-core/*
-git clone --depth=1 https://github.com/oppen321/v2ray-core package/v2ray-core
-cp -af package/v2ray-core/*  feeds/packages/net/v2ray-core/
-
-# shadow-tls
-git clone --depth=1 https://github.com/oppen321/shadow-tls feeds/packages/net/shadow-tls
-
 ## golang 为 1.24.x
 rm -rf feeds/packages/lang/golang/*
 git clone https://github.com/sbwml/packages_lang_golang -b 24.x package/golang
@@ -37,7 +9,6 @@ cp -af package/golang/* feeds/packages/lang/golang/
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
 # luci-app-mosdns
-rm -rf feeds/packages/net/v2ray-geodata/*
 rm -rf feeds/package/net/mosdns/*
 git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
 git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
