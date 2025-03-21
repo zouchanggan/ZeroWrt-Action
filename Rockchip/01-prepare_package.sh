d4f1d4a6b5d4c0415efc8099a63a197fea8e0b2a
OPPEN321 <zj18139624826@gmail.com>
2025-03-21 22:53:02 +0800

    Update 01-prepare_package.sh

--- Rockchip/01-prepare_package.sh
+++ Rockchip/01-prepare_package.sh
@@ -6,6 +6,26 @@
 # Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: openwrt-24.10
 #========================================================================================================================
 
-# golong1.24依赖
+# 替换软件包
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+rm -rf feeds/packages/net/{v2ray-geodata,alist,mentohust,dns2socks,dns2tcp,geoview,sing-box,tcping,trojan,tuic-client,v2ray-core,v2ray-plugin,xray-core.xray-plugin.simple-obfs,shadowsocks-rust,redsocks2,naiveproxy,microsocks,hysteria,lua-neturl}
+rm -rf /feeds/luci/applications/{luci-app-alist,luci-app-mentohust,luci-app-passwall,luci-app-openclash}
+
+# golong1.24依赖
+git clone --depth=1 -b 24.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+
+# helloworld
+git clone --depth=1 -b helloworld https://github.com/oppen321/openwrt-package package/openwrt-package
+
+# mosdns
+git clone --depth=1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone --depth=1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# alist
+git clone --depth=1 https://github.com/sbwml/luci-app-alist package/alist
+
+# mentohust
+git clone --depth=1 https://github.com/sbwml/luci-app-mentohust package/luci-app-mentohust
+
+# adguardhome
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
