7176a198dd52cdfc223480290a5d44d0ca36930d
OPPEN321 <zj18139624826@gmail.com>
2025-03-21 19:59:08 +0800

    Update 00-prepare_base.sh

--- Rockchip/00-prepare_base.sh
+++ Rockchip/00-prepare_base.sh
@@ -1,1 +1,8 @@
+# 修改默认IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
+# TTYD
+sed -i 's/services/system/g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
+sed -i '3 a\\t\t"order": 50,' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
+sed -i 's/procd_set_param stdout 1/procd_set_param stdout 0/g' feeds/packages/utils/ttyd/files/ttyd.init
+sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/utils/ttyd/files/ttyd.init
