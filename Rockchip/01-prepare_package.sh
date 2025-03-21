5c440f9237f004e4b47b3edbba87e40e1e3c5b10
OPPEN321 <zj18139624826@gmail.com>
2025-03-21 21:07:40 +0800

    Update 01-prepare_package.sh

--- Rockchip/01-prepare_package.sh
+++ Rockchip/01-prepare_package.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#========================================================================================================================
+# https://github.com/oppen321/ZeroWrt-Action
+# Description: Automatically Build OpenWrt
+# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: openwrt-24.10
+#========================================================================================================================
 
 # golong1.24依赖
 rm -rf feeds/packages/lang/golang
