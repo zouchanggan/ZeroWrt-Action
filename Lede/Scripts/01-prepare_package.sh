64736d4b5833a92f44ea23233758b230fe3344af
OPPEN321 <zj18139624826@gmail.com>
2025-03-21 19:01:29 +0800

    Delete Lede directory

--- Lede/Scripts/01-prepare_package.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-# TTYD
-sed -i 's/services/system/g' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
-sed -i '3 a\\t\t"order": 50,' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
-sed -i 's/procd_set_param stdout 1/procd_set_param stdout 0/g' feeds/packages/utils/ttyd/files/ttyd.init
-sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/utils/ttyd/files/ttyd.init
-
-#
