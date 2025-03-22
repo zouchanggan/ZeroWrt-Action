a711f496fca9aa5f1e9c5f7051d2cece38430569
OPPEN321 <zj18139624826@gmail.com>
2025-03-22 18:24:09 +0800

    Update build.sh

--- Rockchip/build.sh
+++ Rockchip/build.sh
@@ -4,7 +4,7 @@
 export mirror=http://127.0.0.1:8080
 
 # Clone source code
-git clone -b openwrt-24.10 --single-branch --filter=blob:none https://github.com/immortalwrt/immortalwrt openwrt
+git clone -b openwrt-24.10 --single-branch --filter=blob:none https://github.com/openwrt/openwrt
 
 # Enter source code
 cd openwrt
