ac0a9453a5683ea35b5d8b2e351ef196cd608588
OPPEN321 <zj18139624826@gmail.com>
2025-03-25 13:19:10 +0800

    Update 02-prepare_package.sh

--- Rockchip/02-prepare_package.sh
+++ Rockchip/02-prepare_package.sh
@@ -10,7 +10,6 @@
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/packages/utils/coremark
 rm -rf feeds/packages/net/{zerotier,xray-core,v2ray-core,v2ray-geodata,sing-box}
-rm -rf feeds/luci/applications/luci-app-alist
 
 # golong1.24依赖
 git clone --depth=1 -b 24.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
