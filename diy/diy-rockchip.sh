9f5fbea60a1ca21a21a8afad6208f0d718d699ce
OPPEN321 <zj18139624826@gmail.com>
2025-03-27 18:57:07 +0800

    Update diy-rockchip.sh

--- diy/diy-rockchip.sh
+++ diy/diy-rockchip.sh
@@ -132,6 +132,7 @@ patch -p1 <OpenWrt-Patch/firewall/luci/0002-luci-app-firewall-add-shortcut-fe-op
 patch -p1 <OpenWrt-Patch/firewall/luci/0003-luci-app-firewall-add-ipv6-nat-option.patch
 patch -p1 <OpenWrt-Patch/firewall/luci/0004-luci-add-firewall-add-custom-nft-rule-support.patch
 patch -p1 <OpenWrt-Patch/firewall/luci/0005-luci-app-firewall-add-natflow-offload-support.patch
+patch -p1 <OpenWrt-Patch/firewall/luci/0006-luci-app-firewall-enable-hardware-offload-only-on-de.patch
 patch -p1 <OpenWrt-Patch/firewall/luci/0007-luci-app-firewall-add-fullcone6-option-for-nftables-.patch
 popd
 
