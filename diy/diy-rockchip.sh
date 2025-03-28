8cbc7068d1055bef6a1010916543bc62a9846ee7
OPPEN321 <zj18139624826@gmail.com>
2025-03-28 18:27:38 +0800

    Update diy-rockchip.sh

--- diy/diy-rockchip.sh
+++ diy/diy-rockchip.sh
@@ -3,7 +3,7 @@
 # https://github.com/oppen321/ZeroWrt-Action
 # Description: Automatically Build OpenWrt for Rockchip
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: openwrt-24.10
+# Source code repository: https://github.com/openwrt/openwrt / Branch: openwrt-24.10
 #========================================================================================================================
 
 # Nginx
