c3d53f3465b2f42d9a6fe12b20a911f5318b89a4
OPPEN321 <zj18139624826@gmail.com>
2025-03-28 23:22:07 +0800

    Update diy-x86_64.sh

--- diy/diy-x86_64.sh
+++ diy/diy-x86_64.sh
@@ -71,6 +71,10 @@ git clone --depth=1 -b openwrt-24.10 https://github.com/oppen321/default-setting
 # openwrt patch
 git clone --depth=1 -b kernel-6.6 https://github.com/oppen321/OpenWrt-Patch
 
+
+# make olddefconfig
+wget -qO - https://raw.githubusercontent.com/oppen321/OpenWrt-Patch/refs/heads/kernel-6.6/kernel/0003-include-kernel-defaults.mk.patch | patch -p1
+
 # module
 curl -O https://raw.githubusercontent.com/oppen321/OpenWrt-Patch/refs/heads/kernel-6.6/kernel/0001-linux-module-video.patch
 git apply 0001-linux-module-video.patch
