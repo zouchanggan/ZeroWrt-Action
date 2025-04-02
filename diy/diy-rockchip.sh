491eb25ba1634b6420dd0ad1f5b28dc053758601
OPPEN321 <zj18139624826@gmail.com>
2025-04-02 08:34:38 +0800

    Update diy-rockchip.sh

--- diy/diy-rockchip.sh
+++ diy/diy-rockchip.sh
@@ -206,7 +206,6 @@ sed -i 's#<a class="luci-link" href="https://github.com/openwrt/luci" target="_b
 sed -i 's|<a href="https://github.com/jerrykuku/luci-theme-argon" target="_blank">ArgonTheme <%# vPKG_VERSION %></a>|<a href="https://github.com/oppen321/ZeroWrt-Action" target="_blank">ZeroWrt-Action</a> |g' package/openwrt-package/luci-theme-argon/luasrc/view/themes/argon/footer_login.htm
 
 # 版本设置
-# sed -i "s/\(_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description\) + ' \/ '/\1 + boardinfo.release.revision + ' \/ '/" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
 cat << 'EOF' >> feeds/luci/modules/luci-mod-status/ucode/template/admin_status/index.ut
 <script>
 function addLinks() {
