ab23936989665df86ad6e2fd583e3e5faa88d9a3
OPPEN321 <zj18139624826@gmail.com>
2025-04-06 13:21:23 +0800

    更新 diy-rockchip.sh

--- diy/diy-rockchip.sh
+++ diy/diy-rockchip.sh
@@ -224,6 +224,8 @@ document.addEventListener("DOMContentLoaded", addLinks);
 </script>
 EOF
 
+sed -i "s|_('Firmware Version'), (L.isObject(boardinfo.release) ? boardinfo.release.description + ' / ' : '') + (luciversion || '')|_('Firmware Version'), 'ZeroWrt-VIP-R25.04.6-K6.6.83'|" feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+
 # istoreos
 sed -i 's/iStoreOS/ZeroWrt/' package/openwrt-package/istoreos-files/files/etc/board.d/10_system
 sed -i 's/192.168.100.1/10.0.0.1/' package/openwrt-package/istoreos-files/Makefile
