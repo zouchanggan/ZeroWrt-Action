8877b73efcd9140203f8ff29d2e785b4a15781ae
OPPEN321 <zj18139624826@gmail.com>
2025-04-20 09:59:01 +0800

    up

--- SCRIPTS/Rockchip/03_custom_settings_rk.sh
+++ SCRIPTS/Rockchip/03_custom_settings_rk.sh
@@ -29,26 +29,6 @@ EOF
 # default-settings
 git clone --depth=1 -b aarch64 https://github.com/oppen321/default-settings package/default-settings
 
-mkdir -p files/usr/bin
-
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_amd64 | awk -F '"' '{print $4}')
-
-wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
-
-chmod +x files/usr/bin/AdGuardHome
-
-mkdir -p files/etc/openclash/core
-
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
-GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
-GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-
-wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
-wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
-wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
-
-chmod +x files/etc/openclash/core/clash*
-
 # ZeroWrt选项菜单
 mkdir -p files/bin
 curl -L -o files/bin/ZeroWrt https://git.kejizero.online/zhao/files/raw/branch/main/bin/ZeroWrt
--- SCRIPTS/X86/03_custom_settings_x86.sh
+++ SCRIPTS/X86/03_custom_settings_x86.sh
@@ -48,26 +48,6 @@ src/gz openwrt_telephony https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/2
 src/gz openwrt_core https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/24.10.1/targets/x86/64/kmods/6.6.86-1-af351158cfb5febf5155a3aa53785982
 EOF
 
-mkdir -p files/usr/bin
-
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_amd64 | awk -F '"' '{print $4}')
-
-wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome
-
-chmod +x files/usr/bin/AdGuardHome
-
-mkdir -p files/etc/openclash/core
-
-CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz"
-GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
-GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-
-wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
-wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
-wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
-
-chmod +x files/etc/openclash/core/clash*
-
 # ZeroWrt选项菜单
 mkdir -p files/bin
 curl -L -o files/bin/ZeroWrt https://git.kejizero.online/zhao/files/raw/branch/main/bin/ZeroWrt
