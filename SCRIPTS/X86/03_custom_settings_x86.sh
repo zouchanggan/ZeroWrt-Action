8ed39c1d5c1850ee8bc1a4e84426aff94e30ab20
OPPEN321 <zj18139624826@gmail.com>
2025-04-30 19:09:04 +0800

    Update 03_custom_settings_x86.sh

--- SCRIPTS/X86/03_custom_settings_x86.sh
+++ SCRIPTS/X86/03_custom_settings_x86.sh
@@ -50,6 +50,12 @@ wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
 chmod +x files/etc/openclash/core/clash*
 
+# caddy
+mkdir -p files/usr/bin
+CADDY_URL="https://github.com/lmq8267/caddy/releases/download/v2.10.0/caddy-amd64-upx"
+wget -qO- $CADDY_URL > files/usr/bin/caddy
+chmod +x files/usr/bin/caddy
+
 # 默认设置
 git clone --depth=1 -b openwrt-24.10 https://github.com/oppen321/default-settings package/default-settings
 
