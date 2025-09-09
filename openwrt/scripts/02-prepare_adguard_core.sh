2f5abe1c6c03d94ed22fe6543d18faca21ac5bda
NeonPulse-Zero <3922462371@qq.com>
2025-09-09 10:09:10 +0800

    修复 Adguardhome 内核错误

--- openwrt/scripts/02-prepare_adguard_core.sh
+++ openwrt/scripts/02-prepare_adguard_core.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# 根据平台设置 core 架构
-if [ "$platform" = "rockchip" ]; then
-    core="arm64"
-elif [ "$platform" = "x86_64" ]; then
-    core="amd64"
-fi
-
 mkdir -p files/usr/bin
 
 AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_$core | awk -F '"' '{print $4}')
